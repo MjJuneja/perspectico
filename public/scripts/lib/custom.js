3999fdab2151877a96e3c630ff7512096f11f033
ab-hi <bansal.abhishek101@yahoo.com>
2017-09-04 01:15:15 +0530

    UI updates

--- public/scripts/lib/custom.js
+++ public/scripts/lib/custom.js
@@ -9,35 +9,35 @@
 // });
 
 
-// $(function() {
+$(function() {
     
-//     $('.wrap').addClass('loaded');
+    $('.wrap').addClass('loaded');
     
-//     $('.range').bind('change mousemove', function() {
-//       var val = $(this).val();
-//       var buf = ((100 - val) / 4) + parseInt(val);
-//       $(this).css(
-//         'background',
-//         'linear-gradient(to right, ##4cb6cb 0%, ##4cb6cb ' + val + '%, #777 ' + val + '%, #777 ' + buf + '%, #444 ' + buf + '%, #444 100%)'
-//       );
-//     });
+    $('.range').bind('change mousemove', function() {
+      var val = $(this).val();
+      var buf = ((100 - val) / 4) + parseInt(val);
+      $(this).css(
+        'background',
+        'linear-gradient(to right, ##4cb6cb 0%, ##4cb6cb ' + val + '%, #777 ' + val + '%, #777 ' + buf + '%, #444 ' + buf + '%, #444 100%)'
+      );
+    });
   
-//     var timeout;
-//     $('.wrap').bind('focusin mouseover mousedown hover', function() {
-//       window.clearTimeout(timeout);
-//       $(this).addClass('hover');
-//     });
-//     $('.wrap').bind('focusout mouseout mouseup', function() {
-//       window.clearTimeout(timeout);
-//       timeout = setTimeout(function(){removeHoverClass();}, 1000);
-//     });
-//     function removeHoverClass() {
-//       if (!$('.wrap').is(":hover")) {
-//         $('.wrap').removeClass('hover');
-//       }
-//     }
+    var timeout;
+    $('.wrap').bind('focusin mouseover mousedown hover', function() {
+      window.clearTimeout(timeout);
+      $(this).addClass('hover');
+    });
+    $('.wrap').bind('focusout mouseout mouseup', function() {
+      window.clearTimeout(timeout);
+      timeout = setTimeout(function(){removeHoverClass();}, 1000);
+    });
+    function removeHoverClass() {
+      if (!$('.wrap').is(":hover")) {
+        $('.wrap').removeClass('hover');
+      }
+    }
     
-//   });
+  });
 
 
 // $(document).ready(function(){
